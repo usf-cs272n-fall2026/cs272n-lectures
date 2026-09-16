c6e9b7098848125d7be8a2c61635dc40b61f10f3
Sophie Engle <sjengle@cs.usfca.edu>
2026-09-16 10:48:58 -0700

    Adding cast demo

---
+++ src/main/templates/edu/usfca/cs272/templates/inheritance/CastDemo.java
@@ -0,0 +1,26 @@
+package edu.usfca.cs272.templates.inheritance;
+
+public class CastDemo {
+	public static void main(String[] args) {
+		Number n = Double.valueOf(3.14);
+
+//		Number[] numbers = new Number[] { Double.valueOf(3.14), Integer.valueOf(42), BigInteger.valueOf(123456789) };
+//		Object[] objects = new Object[] { new CastDemo(), new String[] { "a" }, new StringBuilder("potato"), Integer.valueOf(42), List.of(3.14, 2.0) };
+//		CharSequence[] chars = new CharSequence[] { new String("apple"), new StringBuffer("banana"), new StringBuilder("carrot") };
+//		Set<String> set = null;
+
+//		set.add("apple");
+//		set.add("banana");
+//		set.add("carrot");
+
+//		for (String element : set) {
+//			System.out.println(element);
+//		}
+
+//		Object a = new String("3.14");
+//		Double b = (Double) a;
+	}
+
+	// TODO Fill in printFirstLetter
+
+}
